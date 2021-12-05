798ad0b152b6a97875860c9bf41283d5957b7216
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-12-05 23:30:32 +0800

    processfunction

---
+++ Trident/src/main/scala/com/youdi/streamlet/async/HbaseAsyncFunc.scala
@@ -0,0 +1,9 @@
+package com.youdi.streamlet.async
+
+import org.apache.flink.streaming.api.scala.async.{AsyncFunction, ResultFuture}
+
+class HbaseAsyncFunc extends  AsyncFunction[String, String]{
+  override def asyncInvoke(input: String, resultFuture: ResultFuture[String]): Unit = {
+    new HbaseCallback(resultFuture)
+  }
+}
