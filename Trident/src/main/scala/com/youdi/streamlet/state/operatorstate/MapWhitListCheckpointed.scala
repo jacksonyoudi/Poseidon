c4494a8be5dcc009f04eea23205a4b26f6b3439b
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-12-11 08:51:23 +0800

    state function

---
+++ Trident/src/main/scala/com/youdi/streamlet/state/operatorstate/MapWhitListCheckpointed.scala
@@ -0,0 +1,28 @@
+package com.youdi.streamlet.state.operatorstate
+
+import org.apache.flink.api.common.functions.MapFunction
+import org.apache.flink.streaming.api.checkpoint.ListCheckpointed
+
+import java.util
+import java.util.Collections
+
+class MapWhitListCheckpointed[T] extends MapFunction[T, Tuple2[T, Long]] with ListCheckpointed[T] {
+  private var count: Long = _
+
+
+  // 将一个单值返回，会自动
+  override def snapshotState(checkpointId: Long, timestamp: Long): util.List[T] = {
+    Collections.singletonList(count)
+  }
+
+  override def restoreState(state: util.List[T]): Unit = {
+    for (_ <- state.toArray) {
+      count += 1
+    }
+  }
+
+  override def map(value: T): (T, Long) = {
+    count += 1
+    Tuple2(value, count)
+  }
+}
