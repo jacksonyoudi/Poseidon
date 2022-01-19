275365ca7cdb1f85e982e82a20747e485e16464c
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-01-19 22:09:50 +0800

    bean

---
+++ Trident/src/main/scala/org/youdi/app/dws/ProductStatsApp.scala
@@ -0,0 +1,26 @@
+package org.youdi.app.dws
+
+object ProductStatsApp {
+  def main(args: Array[String]): Unit = {
+    //  1. 执行环境
+
+
+    // 读取kafka 7个主题的数据 创建流
+
+
+    // 7个流数据统一格式
+
+
+    // union 7个 流
+
+
+    // 提取时间戳 生成 watermark
+
+
+    // 分组 开窗 聚合  按照 sku_id分组
+
+
+
+  }
+
+}
